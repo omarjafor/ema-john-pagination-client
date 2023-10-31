3a78d5a23db87864f602b86963990200fdccf03f
omarjafor <omarjaforchy@gmail.com>
2023-10-31 23:28:01 +0600

    product count for pegination show

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -9,7 +9,7 @@ const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
     const { count } = useLoaderData();
-    const itemsPerPage = 9;
+    const [itemsPerPage, setItemsPerPage] = useState(9);
     const numberOfPages = Math.ceil(count / itemsPerPage);
 
     // const pages = []
@@ -71,6 +71,11 @@ const Shop = () => {
         deleteShoppingCart();
     }
 
+    const handleItemsPerPage = e => {
+        const val = parseInt(e.target.value);
+        setItemsPerPage(val)
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -96,6 +101,13 @@ const Shop = () => {
                 {
                     pages.map( page => <button key={page}>{page}</button>)
                 }
+                <select value={itemsPerPage} onChange={handleItemsPerPage} name="" id="">
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                    <option value="25">25</option>
+                    <option value="50">50</option>
+                </select>
             </div>
         </div>
     );
