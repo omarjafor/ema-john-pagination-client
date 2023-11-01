91013047d0bbbb559649b16db7dac76f1e2ff1ca
omarjafor <omarjaforchy@gmail.com>
2023-11-01 15:29:25 +0600

    shoping cart data finding by ids

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,8 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Shop></Shop>,
-        loader: () => fetch('http://localhost:5000/productCount')
+        loader: cartProductsLoader
+        // loader: () => fetch('http://localhost:5000/productCount')
       },
       {
         path: 'orders',
