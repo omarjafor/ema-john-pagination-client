1ad3953e80a9fcdbf9e21850bf4f08818232ffd3
omarjafor <omarjaforchy@gmail.com>
2023-11-01 16:27:14 +0600

    shoping cart data finding by ids

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -34,7 +34,25 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [currentPage, itemsPerPage]);
 
-    
+    // useEffect(() => {
+    //     const storedCart = getShoppingCart();
+    //     const savedCart = [];
+    //     // step 1: get id of the addedProduct
+    //     for (const id in storedCart) {
+    //         // step 2: get product from products state by using id
+    //         const addedProduct = products.find(product => product._id === id)
+    //         if (addedProduct) {
+    //             // step 3: add quantity
+    //             const quantity = storedCart[id];
+    //             addedProduct.quantity = quantity;
+    //             // step 4: add the added product to the saved cart
+    //             savedCart.push(addedProduct);
+    //         }
+    //         // console.log('added Product', addedProduct)
+    //     }
+    //     // step 5: set the cart
+    //     setCart(savedCart);
+    // }, [products])
 
     const handleAddToCart = (product) => {
         // cart.push(product); '
